824ae35fea3275b767073ca6f61c603015403d28
ECiarabellini <emilyciarabellini@gmail.com>
2023-07-09 16:12:31 -0700

    Add functional reset button

--- main.js
+++ main.js
@@ -6,11 +6,19 @@ for (let n=0; n<256; n++){
     newSquare.classList.add('squares');
     pad.appendChild(newSquare);
 }
-let squares = document.querySelectorAll('.squares');
 
+let squares = document.querySelectorAll('.squares');
 squares.forEach((square) =>{
     square.addEventListener('mouseover', () => {
-        square.setAttribute('style','background-color:white;')
+        square.setAttribute('style','background-color:#008080;')
     });
-
 });
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) =>{
+    button.addEventListener('click', () => console.log(button));
+    
+});
+
+const resetButton = document.querySelector("#reset");
+resetButton.addEventListener('click', () => location.reload());
