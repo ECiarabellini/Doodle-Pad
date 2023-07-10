15c211c1dd7235667e700873a63065b4c57490e3
ECiarabellini <emilyciarabellini@gmail.com>
2023-07-10 15:38:40 -0700

    Adding all files for troubleshooting

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 const pad = document.querySelector("#pad");
 let newSquare;
 
-for (let n=0; n<256; n++){
-    newSquare = document.createElement('div');
-    newSquare.classList.add('squares');
-    pad.appendChild(newSquare);
-}
+let squareSize = "medium";
+let numSmallSquares = 1600;
+let numMediumSquares = 256;
+let numLargeSquares = 16;
 
-let squares = document.querySelectorAll('.squares');
+let squareClass;
+let numSquares;
+console.log("squareClass is: "+squareClass);
+console.log("numSquares is "+ squareClass);
+
+if (squareSize = "small"){
+    numSquares = numSmallSquares;
+    squareClass = ".smallSquares";
+} else if (squareSize = "medium"){
+    numSquares = numMediumSquares;
+    squareClass = ".mediumSquares";
+} else {
+    numSquares = numLargeSquares;
+    squareClass = ".largeSquares";
+};
+
+console.log("squareClass is: "+squareClass);
+console.log("numSquares is "+ numSquares);
+
+
+for (let n=0; n<numSquares; n++){
+    newSquare = document.createElement('div');
+    newSquare.classList.add(squareClass);
+    pad.appendChild(newSquare);
+};
+
+let squares = document.querySelectorAll(squareClass);
 squares.forEach((square) =>{
     square.addEventListener('mouseover', () => {
         square.setAttribute('style','background-color:#008080;')
@@ -17,8 +42,16 @@ squares.forEach((square) =>{
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) =>{
     button.addEventListener('click', () => console.log(button));
-    
 });
 
 const resetButton = document.querySelector("#reset");
 resetButton.addEventListener('click', () => location.reload());
+
+
+
+// number of squares --> 400px wide box
+// 265 (16x16) --> 25px height and innerWidth
+// .
+// .
+// ...
+
