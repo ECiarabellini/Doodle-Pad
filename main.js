b33e8b8bb12f76380cb61244c5e4f1078ee75de5
ECiarabellini <emilyciarabellini@gmail.com>
2023-07-10 17:24:16 -0700

    Add functional small/med/large buttons

--- main.js
+++ main.js
@@ -1,55 +1,64 @@
-const pad = document.querySelector("#pad");
-let newSquare;
-
-let squareSize = "medium";
-let numSmallSquares = 1600;
-let numMediumSquares = 256;
-let numLargeSquares = 16;
-
-let squareClass = "mediumSquares";
-let numSquares = 256;
-console.log("squareSize is: " + squareSize)
-console.log("squareClass is: " + squareClass);
-console.log("numSquares is " + numSquares);
-
-if (squareSize === "small"){
-    numSquares = numSmallSquares;
-    squareClass = "smallSquares";
-} else if (squareSize === "large"){
-    numSquares === numLargeSquares;
-    squareClass = "largeSquares";
-} else {
-    numSquares = numMediumSquares;
-    squareClass = "mediumSquares";
-};
-
-console.log("squareSize is: " + squareSize)
-console.log("squareClass is: " + squareClass);
-console.log("numSquares is " + numSquares);
-
-
-for (let n=0; n<numSquares; n++){
-    newSquare = document.createElement('div');
-    newSquare.classList.add(squareClass);
-    pad.appendChild(newSquare);
-};
-
-let squares = document.querySelectorAll("."+squareClass);
-squares.forEach((square) =>{
-    square.addEventListener('mouseover', () => {
-        square.setAttribute('style','background-color:#008080;')
-    });
-});
-
-const mediumButton = document.querySelector('#medium');
-mediumButton.setAttribute('style', 'background-color: darkmagenta;');
-
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) =>{
-    button.addEventListener('click', () => console.log(button));
-});
-
 const resetButton = document.querySelector("#reset");
 resetButton.addEventListener('click', () => location.reload());
 
+const buttonSmall = document.querySelector('#small');
+const buttonMedium = document.querySelector('#medium');
+const buttonLarge = document.querySelector('#large');
+buttonMedium.setAttribute('style', 'background-color: darkmagenta;');
 
+const pad = document.querySelector("#pad");
+let squareClass; 
+let numSquares;
+
+function createGrid(size){
+    pad.replaceChildren();
+    if (size == "small"){
+        numSquares = 1600;
+        squareClass = "smallSquares";
+        buttonSmall.setAttribute('style', 'background-color: darkmagenta;');
+        buttonLarge.setAttribute('style', 'background-color: antiquewhite;');
+        buttonMedium.setAttribute('style', 'background-color: antiquewhite;');
+    } else if (size == "large"){
+        numSquares = 16;
+        squareClass = "largeSquares";
+        buttonLarge.setAttribute('style', 'background-color: darkmagenta;');
+        buttonSmall.setAttribute('style', 'background-color: antiquewhite;');
+        buttonMedium.setAttribute('style', 'background-color: antiquewhite;');
+    } else if (size == "medium"){
+        numSquares = 256;
+        squareClass = "mediumSquares";
+        buttonMedium.setAttribute('style', 'background-color: darkmagenta;');
+        buttonSmall.setAttribute('style', 'background-color: antiquewhite;');
+        buttonLarge.setAttribute('style', 'background-color: antiquewhite;');
+    };
+    for (let n=0; n<numSquares; n++){
+        let newSquare = document.createElement('div');
+        newSquare.classList.add(squareClass);
+        pad.appendChild(newSquare);
+    };
+    setDrawColor(squareClass);
+    console.log("2.squareClass is: " + squareClass);
+    console.log("2.numSquares is " + numSquares);
+};
+
+
+function setDrawColor (squareClass) {
+    let squares = document.querySelectorAll("."+squareClass); 
+    squares.forEach((square) =>{
+        square.addEventListener('mouseover', () => {
+            square.setAttribute('style','background-color:#008080;')
+        });
+    });
+};
+
+
+createGrid("medium");
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) =>{button.addEventListener('click', () => {
+    console.log(button.innerText);
+    createGrid(button.innerText);
+    })});
+
+// buttons.forEach((button) =>{
+//     button.addEventListener('click', function(e) {console.log(e);});});
